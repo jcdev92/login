a1c4cb43d1c448abc2727fa130f449be43f2e293
jcdev92 <jcdeveloper92@gmail.com>
2022-11-21 22:37:56 -0400

    log in painted

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,9 +1,40 @@
-import { Text , View } from "react-native";
+import { StyleSheet , Text , TextInput , View } from "react-native";
 
 export const Login = () => {
     return (
         <View>
-            <Text>Login</Text>
+            <Text style={styles.title}>Hello</Text>
+            <Text style={styles.subTitle}>Sign in to your account</Text>
+            <TextInput placeholder="your@email.com" style={styles.inputText}/>
+            <TextInput placeholder="Password" style={styles.inputText}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+                                     title: {
+                                         fontSize: 80 ,
+                                         color: "#34434D" ,
+                                         fontWeight: "Bold" ,
+
+                                     } ,
+                                     subTitle: {
+                                         fontSize: 20 ,
+                                         color: "grey" ,
+                                     } ,
+                                     inputText: {
+                                         fontSize: 20 ,
+                                         color: "grey" ,
+                                         borderWidth: 1 ,
+                                         padding: 10 ,
+                                         borderRadius: 30 ,
+                                         width: 300 ,
+                                         height: 50 ,
+                                         marginTop: 20 ,
+                                         backgroundColor: "#fff" ,
+                                         paddingStart: 20 ,
+                                     }
+
+                                 })
+
+
